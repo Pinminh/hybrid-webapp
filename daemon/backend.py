44082e122a09d6f28e928e537cabce4f88c82790
Pinminh <minh.nguyenhongk22@hcmut.edu.vn>
2025-11-02 09:24:19 +0700

    Add thread handling in backend

--- daemon/backend.py
+++ daemon/backend.py
@@ -89,7 +89,10 @@ def run_backend(ip, port, routes):
             #  TODO: implement the step of the client incomping connection
             #        using multi-thread programming with the
             #        provided handle_client routine
-            #
+            #  DONE
+            client_thread = threading.Thread(target=handle_client, args=(ip, port, conn, addr, routes))
+            client_thread.daemon = True
+            client_thread.start()
     except socket.error as e:
         print("Socket error: {}".format(e))
 
