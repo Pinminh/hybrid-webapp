f8e7bc7f871fc0f672f3b8a1a37c2ca3cdd0c543
thinhphan2313306 <thinh.phan2313306@hcmut.edu.vn>
2025-11-07 08:29:23 +0700

    update task_2

--- start_sampleapp.py
+++ start_sampleapp.py
@@ -27,9 +27,10 @@ and can be configured via command-line arguments.
 import json
 import socket
 import argparse
-
+peer_list = []
 from daemon.weaprous import WeApRous
-
+from db import peer_list
+from db import active_peers
 PORT = 8000  # Default port
 
 app = WeApRous()
@@ -59,6 +60,79 @@ def hello(headers, body):
     :param body (str): The request body or message payload.
     """
     print("[SampleApp] ['PUT'] Hello in {} to {}".format(headers, body))
+
+
+
+@app.route("/submit-info", methods=["POST"])
+def submit_info(headers=None, body=None):
+    """
+    Peer submits info to central tracker
+    Expected format: ip=...&port=...
+    """
+
+    print(f"[Submit] Received peer info: ")
+
+
+peer_list = []  # simple in-memory tracker
+
+@app.route("/add-list", methods=["POST"])
+def add_list(headers=None, body=None):
+    """
+    Add a peer to active list
+    Example body: ip=127.0.0.1&port=9002
+    """
+    params = dict(pair.split("=", 1) for pair in body.split("&") if "=" in pair)
+    ip = params.get("ip", "")
+    port = params.get("port", "")
+    if ip and port:
+        peer_list.append((ip, port))
+        print(f"[SampleApp] Peer added to list: {ip}:{port}")
+    else:
+        print("[SampleApp] Failed to added peer to list")
+
+
+@app.route("/get-list", methods=["GET"])
+def get_list(headers=None, body=None):
+    """
+    Return list of all active peers
+    """
+    print("[SampleApp] Get-List in {} to {}".format(headers, body))
+
+
+
+@app.route("/connect-peer", methods=["POST"])
+def connect_peer(headers=None, body=None):
+    """
+    Initiate connection to another peer
+    """
+    params = dict(pair.split("=", 1) for pair in body.split("&") if "=" in pair)
+    target = params.get("target", "")
+    print(f"[SampleApp] Connecting to peer {target}")
+
+
+@app.route("/broadcast-peer", methods=["POST"])
+def broadcast_peer(headers=None, body=None):
+    """
+    Broadcast message to all peers
+    """
+    params = dict(pair.split("=", 1) for pair in body.split("&") if "=" in pair)
+    message = params.get("message", "")
+    print(f"[SampleApp] Broadcasting message: {message}")
+    print(f"   → Sent to {len(peer_list)} peers")
+    print(f"Broadcasted message to {len(peer_list)} peers.")
+
+
+
+@app.route("/send-peer", methods=["POST"])
+def send_peer(headers=None, body=None):
+    """
+    Send message directly to one peer
+    Example body: target=127.0.0.1:9002&message=Hello
+    """
+    params = dict(pair.split("=", 1) for pair in body.split("&") if "=" in pair)
+    target = params.get("target", "")
+    message = params.get("message", "")
+    print(f"[SampleApp] Send to {target}: {message}")
 
 if __name__ == "__main__":
     # Parse command-line arguments to configure server IP and port
