ed81a576d9dba4d6877e0a3eaec654953c287e78
thinhphan2313306 <thinh.phan2313306@hcmut.edu.vn>
2025-11-08 09:39:31 +0700

    Add files via upload

---
+++ db/__init__.py
@@ -0,0 +1,8 @@
+# đường dẫn db/__init__.py
+# Khởi tạo danh sách lưu các peer kết nối
+peer_list = []
+active_peers = []
+connections = []
+chat_history = {}  # ánh xạ cặp peer -> list message
+pending_requests = []  # chứa các yêu cầu chờ xác nhận
+history_chat = {}     # { "ipA:portA|ipB:portB" : [ { "sender": "...", "msg": "..." }, ..
