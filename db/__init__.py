86f56fd9359b2923e13339f696d9e1c0cc5bf447
thinhphan2313306 <thinh.phan2313306@hcmut.edu.vn>
2025-11-10 00:37:43 +0700

    Add files via upload

--- db/__init__.py
+++ db/__init__.py
@@ -6,7 +6,7 @@ connections = {}
 chat_history = {}  # ánh xạ cặp peer -> list message
 pending_requests = []  # chứa các yêu cầu chờ xác nhận
 history_chat = {}     # { "ipA:portA|ipB:portB" : [ { "sender": "...", "msg": "..." }, ..
-
+notifications = {}
 from .session import (
     Session,
     SessionManager,
